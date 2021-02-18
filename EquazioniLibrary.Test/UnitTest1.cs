94ba7f691eae123deaf1c122ae13df4eaf3ec3bc
alessandro <m.cherubini52@gmail.com>
2021-02-18 12:58:03 +0100

    Aggiungere i file di progetto.

---
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestEquazioniPrimoGrado()
+        {
+            double a = 1;
+            bool valoreAspettato = true;
+            bool controlloValore = Equazioni.IsDetermined(a);
+            Assert.AreEqual(valoreAspettato, controlloValore);
+        }
+        [TestMethod]
+        public void TestIncosisted()
+        {
+            double a = 1;
+            double b = 3;
+            bool valoreAspettato = true;
+            bool controlloValore = Equazioni.IsIncosisted(a, b);
+            Assert.AreEqual(valoreAspettato, controlloValore);
+        }
+        [TestMethod]
+        public void TestDegree2()
+        {
+            double a = 0;
+            bool valoreAspettato = false;
+            bool controlloValore = Equazioni.IsDegree2(a);
+            Assert.AreEqual(valoreAspettato, controlloValore);
+        }
+
+
+    }
+}
---
+++ EquazioniLibrary/Equazioni.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EquazioniLibrary
+{
+    public class Equazioni
+    { 
+        public static bool IsDetermined(double a)
+        {
+            bool valore = false;
+            if(a!=0)
+            {
+                valore = true;
+            }
+            return valore;
+        }
+        public static bool IsIncosisted (double a,double b)
+        {
+            bool valore = false;
+            if (a!=0)
+            {
+                if(b!=0)
+                {
+                    valore = true;
+                }
+            }
+            return valore;
+        }
+        public static bool IsDegree2(double a )
+        {
+            bool valore = false;
+            if(a==0)
+            {
+                return valore;
+            }
+            else
+            {
+                valore = true;
+            }
+            return valore;
+        }
+    }
+}
