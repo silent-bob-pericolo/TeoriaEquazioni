06f0474aefa21ba54998c07380c0796915b4728b
alessandro <m.cherubini52@gmail.com>
2021-03-04 13:50:29 +0100

    ultimi punti 5 e 6

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -40,6 +40,77 @@ namespace EquazioniLibrary.Test
             double controlloValore = Equazioni.Delta(a, b, c);
             Assert.AreEqual(valoreAspettato, controlloValore);
         }
+        [TestMethod]
+        public void Rsoluzione()
+        {
+            double a = 12;
+            double b = 12;
+            double c = 3;
+            double risultato = -1.0 / 2;
+            string valoreAspettato = ($"una sola soluzione che è{risultato}");
+            string controlloValore = Equazioni.RisoluzioneEquazioneSecondoGrado(a, b, c);
+            Assert.AreEqual(valoreAspettato, controlloValore);
+        }
+        [TestMethod]
+        public void Risoluzione2()
+        {
+            double a = 1;
+            double b = 5;
+            double c = 6;
+            string valoreAspettato = ($"due soluzione che sono {-2} e {-3}");
+            string controlloValore = Equazioni.RisoluzioneEquazioneSecondoGrado(a, b, c);
+            Assert.AreEqual(valoreAspettato, controlloValore);
+        }
+        [TestMethod]
+        public void Risoluzione3()
+        {
+            double a = 0;
+            double b = 5;
+            double c = 6;
+            string valoreAspettato = "non eseguibile";
+            string controlloValore = Equazioni.RisoluzioneEquazioneSecondoGrado(a, b, c);
+            Assert.AreEqual(valoreAspettato, controlloValore);
+        }
+        [TestMethod]
+        public void Risoluzione4()
+        {
+            double a = 2;
+            double b = 0;
+            double c = 2;
+            string valoreAspettato =$"nessuna soluzione perchè è impossibile" ;
+            string controlloValore = Equazioni.RisoluzioneEquazioneSecondoGrado(a, b, c);
+            Assert.AreEqual(valoreAspettato, controlloValore);
+        }
+        [TestMethod]
+        public void testEquazionePrimoGrado()
+        {
+            double a = 0;
+            double b = 2;
+            string valoreAspettato = ("l'equazione è impossibile");
+            string controllo = Equazioni.EquationDegree1(a, b);
+            Assert.AreEqual(valoreAspettato, controllo);
+        }
+        [TestMethod]
+        public void testEquazionePrimoGrado2()
+        {
+            double a = -2;
+            double b = 4;
+            string valoreAspettato = ("la soluzione è -2");
+            string controllo = Equazioni.EquationDegree1(a, b);
+            Assert.AreEqual(valoreAspettato, controllo);
+        }
+        [TestMethod]
+        public void testEquazionePrimoGrado3()
+        {
+            double a = 0;
+            double b = 0;
+            string valoreAspettato = ("l'equazione è indeterminata");
+            string controllo = Equazioni.EquationDegree1(a, b);
+            Assert.AreEqual(valoreAspettato, controllo);
+        }
+
+
+
 
 
 
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -43,5 +43,45 @@ namespace EquazioniLibrary
             double delta = b * b - 4 * a * c;
             return delta;
         }
+        public static string RisoluzioneEquazioneSecondoGrado(double a, double b, double c)
+        {
+            double delta;
+            delta = Delta(a, b, c);
+            if (delta == 0)
+            {
+                double x1 = -(b) / (2 * a);
+                return ($"una sola soluzione che è {x1}");
+            }
+            else if (delta < 0)
+            {
+                return ($"nussuna soluzione perchè è impossibile");
+            }
+            else if (delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+                return ($"due soluzioni che sono {x1} e {x2}");
+            }
+            else
+            {
+                return ("non eseguibile");
+            }
+        }
+        public static string EquationDegree1(double a,double b)
+        {
+            if (IsDetermined(a))
+            {
+                double x = b / a;
+                return ($"la soluzione dell'equazione è {x}");
+            }
+            else if (IsIncosisted(a,b))
+            {
+                return ("l'equazione è impossibile");
+            }
+            else
+            {
+                return ("l'equazione è indeterminata");
+            }
+        }
     }
 }
